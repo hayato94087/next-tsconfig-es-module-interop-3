18949d4f8e69c593665c3901ba68341e2d1e5448
hayato94087 <hayato94087@gmail.com>
2024-03-24 07:32:44 +0900

    feat:esModuleInteropを無効化しエラーを確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import snakecaseKeys from "snakecase-keys";
 import { type FC } from "react";
 
 const Home: FC = () => {
+  console.log(snakecaseKeys({ userName: "Taro Yamada" }));
+
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
